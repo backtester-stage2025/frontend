47d2f2dce79139485fa8a64fbe93d3ff53711d1f
Thorsten Dobbelaere <thorsten.dobbelaere@student.kdg.be>
2025-04-22 13:26:45 +0200

    Added default route, header + nav, made stock list flex, fixed root position

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import axios from "axios";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {StockList} from "./components/StockList.tsx";
+import {useState} from "react";
+import {Header} from "./components/Header.tsx";
+import {Navigation} from "./components/Navigation.tsx"
 
 function App() {
   axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
   const queryClient = new QueryClient()
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+          <Header onOpenDrawer={() => setDrawerOpen(!drawerOpen)}/>
+          <Navigation isOpen={drawerOpen} onClose={() => setDrawerOpen(false)}/>
           <Routes>
               <Route path="/list" element={<StockList/>} />
+              <Route path="/" element={<StockList/>} />
           </Routes>
       </BrowserRouter>
     </QueryClientProvider>
--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -14,31 +14,34 @@ export function StockList() {
     }
 
     return (
+        <div>
+            <h1>Available Stocks</h1>
+            <Box sx={{
+                width: '80vw',
+                maxWidth: 'lg',
+                display: 'flex',
+                flexWrap: 'wrap'
+            }}>
 
-        <Box sx={{
-            width: '80vw',
-            maxWidth: 'lg'
-        }}>
-            {
-                stockData.map((item: string) => (
-                    <Card sx={{
-                        p: 3,
-                        m: 1,
-                        minWidth: '75%',
-                        boxShadow: 3,
-                        // Hover effect
-                        transition: '0.3s',
-                        cursor: 'pointer',
-                        '&:hover': {
-                            boxShadow: 6,
-                            backgroundColor: 'rgba(0, 0, 0, 0.2)',
-                        }
-                    }} key={item}>{item}</Card>
-                ))
-            }
-        </Box>
-
-
+                {
+                    stockData.map((item: string) => (
+                        <Card sx={{
+                            p: 3,
+                            m: 1,
+                            minWidth: '25%',
+                            boxShadow: 3,
+                            // Hover effect
+                            transition: '0.3s',
+                            cursor: 'pointer',
+                            '&:hover': {
+                                boxShadow: 6,
+                                backgroundColor: 'rgba(0, 0, 0, 0.2)',
+                            }
+                        }} key={item}>{item}</Card>
+                    ))
+                }
+            </Box>
+        </div>
 
 
     )
