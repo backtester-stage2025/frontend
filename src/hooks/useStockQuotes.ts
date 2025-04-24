e3ccc21a52faa0f559ca57f4c53546ffbe2e7b8f
Thorsten Dobbelaere <thorsten.dobbelaere@student.kdg.be>
2025-04-24 14:20:51 +0200

    Made queries inline

--- src/hooks/useStockQuotes.ts
+++ src/hooks/useStockQuotes.ts
@@ -2,12 +2,11 @@ import {useQuery} from "@tanstack/react-query";
 import {getStockQuotes} from "../services/stockDataService";
 
 export function useStockQuotes(stockName: string) {
-    const query = {
+    const {isLoading, isError, data: stockQuotes} = useQuery({
         queryKey: ['stockQuotes', stockName],
         queryFn: () => getStockQuotes(stockName),
         enabled: !!stockName
-    }
-    const {isLoading, isError, data: stockQuotes} = useQuery(query);
+    });
     return {
         isLoading,
         isError,
