48773484ece19050788c2f6a6ff884dfd663c4ea
DevStefaan <vosstef@cronos.be>
2025-05-02 18:18:14 +0200

    Creating StockMetricsContent for better readability

---
+++ src/components/simulation/results/metrics/StockMetricsContent.tsx
@@ -0,0 +1,27 @@
+import {Loader} from "../../../util/Loader.tsx";
+import {StockMetricsView} from "./StockMetricsView.tsx";
+import {Box, Typography} from "@mui/material";
+import {SimulationReport} from "../../../../model/simulation/SimulationReport.ts";
+
+interface StockMetricsContentProps {
+    isLoadingReport: boolean;
+    simulationReport: SimulationReport | undefined;
+}
+
+export function StockMetricsContent({isLoadingReport, simulationReport}: Readonly<StockMetricsContentProps>) {
+
+    if (isLoadingReport)
+        return <Loader/>
+
+    if (simulationReport) {
+        return <StockMetricsView stockMetrics={simulationReport.stockMetrics}/>
+    } else {
+        return (
+            <Box sx={{p: 3, textAlign: 'center'}}>
+                <Typography color="text.secondary">
+                    No stock metrics available. Please run a simulation first.
+                </Typography>
+            </Box>
+        )
+    }
+}
