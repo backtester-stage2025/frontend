4a82831582693610283492bc10d4df58d7770f87
Olaf Siudak <olaf.siudak@student.kdg.be>
2025-05-26 09:46:36 +0200

    Added links from tooltips to stockmetrics info

--- src/constants/tooltipMessages.ts
+++ src/constants/tooltipMessages.ts
@@ -27,12 +27,20 @@ export const TOOLTIP_MESSAGES = {
             " [0% for no buffer]"
     },
     stockMetrics: {
+        returnsTitle: "Returns",
         returnsInfo: "The average percentage daily and annual return of the stock over the simulation period.",
+        returnsLink: "/infopages?section=returns",
+        riskTitle: "Risk",
         riskInfo: "The average percentage daily and annual risk of the stock over the simulation period.",
+        riskLink: "/infopages?section=risk",
+        drawdownsTitle: "Drawdown",
         drawdownsInfo: "The average and maximum percentage drawdown of the stock over the simulation period." +
             " This is the difference between the peak and lowest price.",
+        drawdownsLink: "/infopages?section=drawdown",
+        distributionTitle: "Skewness",
         distributionInfo: "The skewness of the stock returns over the simulation period." +
             " A positive skewness indicates a higher probability of positive returns," +
-            " while a negative skewness indicates a higher probability of negative returns."
+            " while a negative skewness indicates a higher probability of negative returns.",
+        distributionLink: "/infopages?section=skewness"
     }
 }
