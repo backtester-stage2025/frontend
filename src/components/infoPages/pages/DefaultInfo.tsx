89f95800b3000fe9d73c9a4b72c1674cc07f02f2
Olaf Siudak <olaf.siudak@student.kdg.be>
2025-05-26 10:05:50 +0200

    Added exmaple tooltip

--- src/components/infoPages/pages/DefaultInfo.tsx
+++ src/components/infoPages/pages/DefaultInfo.tsx
@@ -1,4 +1,6 @@
-import {Box, Container, Divider, Typography} from "@mui/material";
+import {Box, Container, Divider, IconButton, Typography} from "@mui/material";
+import {TooltipHtml} from "../../util/TooltipHtml.tsx";
+import InfoIcon from "@mui/icons-material/Info";
 
 export function DefaultInfo() {
     return (
@@ -16,6 +18,23 @@ export function DefaultInfo() {
                     These pages provide detailed explanations and examples to help you understand different trading
                     strategies and concepts.
                     Use the navigation menu to explore topics like Moving Average Crossovers or Breakouts.
+                </Typography>
+                <Typography variant="h6" gutterBottom sx={{mt: 4}}>
+                    Tooltips
+                    <TooltipHtml
+                        title="Learn More About Tooltips"
+                        description="Tooltips provide additional information about specific terms and concepts throughout the app."
+                        link="/infopages"
+                    >
+                        <IconButton sx={{ marginRight: 1, color: 'rgba(0, 0, 0, 0.3)'}}>
+                            <InfoIcon sx={{ fontSize: '1.4rem' }} />
+                        </IconButton>
+                    </TooltipHtml>
+                </Typography>
+                <Typography variant="body1" sx={{mt: 2}}>
+                    Throughout the app, you will find tooltips that provide additional information about
+                    specific terms and concepts. Some of these have a title (like the example above)
+                    which you can click through on to go to the relevant info page for more details.
                 </Typography>
             </Box>
         </Container>
