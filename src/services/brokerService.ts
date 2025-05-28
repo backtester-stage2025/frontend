ffd589812eecf1ab6c32236d61825d952415847c
DevStefaan <vosstef@cronos.be>
2025-05-28 10:11:58 +0200

    Removing baseUrl from broker and stock service

--- src/services/brokerService.ts
+++ src/services/brokerService.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-
 export async function getBrokers() {
-    const {data: brokers} = await axios.get(`${BASE_URL}/api/brokers/all`);
+    const {data: brokers} = await axios.get(`/api/brokers/all`);
 
     return brokers;
 }
