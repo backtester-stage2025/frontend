f255df894031f42fae72dbf0dc73e98c1186ee82
Thorsten Dobbelaere <thorsten.dobbelaere@student.kdg.be>
2025-05-30 09:33:07 +0200

    Updated condition to overwrite uploaded file

--- src/components/stockList/csv/CsvUploadDialog.tsx
+++ src/components/stockList/csv/CsvUploadDialog.tsx
@@ -173,7 +173,7 @@ export function CsvUploadDialog({open, onClose}: Readonly<CsvUploadDialogProps>)
                     onClose();
                 },
                 onError: (error) => {
-                    if (error?.message.includes("already exists. Please use a different name.")) {
+                    if (error?.message.includes("continue")) {
                         setShowOverwriteDialog(true);
                     } else {
                         setShowError(true);
@@ -270,7 +270,7 @@ export function CsvUploadDialog({open, onClose}: Readonly<CsvUploadDialogProps>)
                                 value={form.currencyType}
                                 onChange={handleCurrencyChange}
                                 disabled={isRunning}
-                            >
+                                variant={"outlined"}>
                                 {Object.values(CurrencyType).map((currency) => (
                                     <MenuItem key={currency} value={currency}>
                                         {currency}
